70db405384dbb95baf488fb09fff947f068816de
rodrig <isoschronos@gmail.com>
2024-10-24 20:28:21 -0500

    Graph path

--- audiomancy/constants.py
+++ audiomancy/constants.py
@@ -11,6 +11,7 @@ ABSOLUTE_PATH = os.path.join(os.path.dirname(__file__))
 DATASETS_PATH = os.path.join(ABSOLUTE_PATH,'common','datasets')
 STEMS_PATH = os.path.join(ABSOLUTE_PATH,'common','stems')
 TEMP_PATH = os.path.join(ABSOLUTE_PATH,'common','temp')
+GRAPH_PATH = os.path.join(os.path.abspath(os.path.join(ABSOLUTE_PATH,'..')),'graphs')
 
 JAMS_FILE_200 = os.path.join(ABSOLUTE_PATH,'common','200_soundscapes.jams')
 JAMS_FILE_1000 = os.path.join(ABSOLUTE_PATH,'common','1000_soundscapes.jams')
